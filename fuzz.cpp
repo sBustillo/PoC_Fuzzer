f0053ebe248e811e91c5fc00dd0d7068e3450d8e
Samuel Bustillo Remeseiro <32135641+sBustillo@users.noreply.github.com>
2021-12-16 21:29:19 +0100

    Definition and Analysis of problem -

---
+++ fuzz.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main(){
+	
+	#preguntamos por num de archivos N a generar
+	
+	
+	
+	#preguntamos por la longitud L de las strings a insertar
+	
+	
+	#preguntamos por el archivo para mutar
+	
+	
+	
+	#abrimos el archivo y hacemos copia del mismo en memoria
+	
+	
+	#FUNCION: 		
+	
+		#puntero random al archivo copia en memoria		
+		
+		#generamos string de bytes de longitud L y lo escribimos en la direccion que nos devolvio el puntero en la instruccion anterior 
+		
+		#guardamos los cambios en un fichero en el sistema
+		
+		#BORRAMOS el archivo copia de memoria (el archivo y el puntero y to')
+		
+		#si no hemos llegado a N iter vuelve a llamarse a si misma
+
+	return 0;	
+}
